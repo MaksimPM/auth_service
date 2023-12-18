4930c4ba77b23edb0163f47accae0bfc47a4d74b
Maksim Pakhomov <maksim-paxomov-2000@mail.ru>
2023-12-18 08:43:57 +0300

    6 commit

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from rest_framework import routers
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import UserRegister, UserListAPIView, UserDestroyAPIView, UserAuthorizationView, PasswordRecoveryView, \
-    RequestPasswordRecoveryView, UserUpdateAPIView
+from users.views import UserRegister, UserListAPIView, UserAuthorizationView, PasswordRecoveryView, \
+    RequestPasswordRecoveryView
 
 app_name = UsersConfig.name
 
@@ -14,12 +13,7 @@ urlpatterns = [
     path('', UserListAPIView.as_view(), name='users'),
     path('sign-up/', UserRegister.as_view(), name='user_register'),
     path('sign-in/', UserAuthorizationView.as_view(), name='user_authorization'),
-    path('update/<int:pk>/', UserUpdateAPIView.as_view(), name='user_update'),
-    path('<int:pk>/delete/', UserDestroyAPIView.as_view(), name='user_delete'),
 
     path('recovery/', RequestPasswordRecoveryView.as_view(), name='password_recovery'),
     path('recovery/<str:hash>/', PasswordRecoveryView.as_view(), name='password_recovery'),
-
-    path('token/', TokenObtainPairView.as_view(), name='token'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ] + router.urls
--- users/views.py
+++ users/views.py
@@ -1,11 +1,9 @@
 from rest_framework import generics, status
-from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 
 from users.models import User
-from users.permissions import IsOwnerProfile
 from users.serializers import UserSerializer, PasswordRecoverySerializer, RequestPasswordRecoverySerializer, \
     AuthorizationSerializer
 from users.tasks import send_email_task, send_password_reset_email
@@ -21,17 +19,6 @@ class UserRegister(generics.CreateAPIView):
 
 class UserListAPIView(generics.ListAPIView):
     serializer_class = UserSerializer
-    queryset = User.objects.all()
-
-
-class UserUpdateAPIView(generics.UpdateAPIView):
-    serializer_class = UserSerializer
-    permission_classes = (IsAuthenticated, IsOwnerProfile)
-    queryset = User.objects.all()
-
-
-class UserDestroyAPIView(generics.DestroyAPIView):
-    permission_classes = (IsAuthenticated, IsOwnerProfile,)
     queryset = User.objects.all()
 
 
